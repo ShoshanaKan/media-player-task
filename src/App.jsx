ac7598be69ce221163b270f6096159661ef3b79f
ShoshanaKan <sbd1057@gmail.com>
2025-06-03 22:51:16 +0300

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
-import { useState } from "react";
 import { meeting } from "./data/mockMeetingData";
 import "./App.css";
 import MediaPlayer from "./components/MediaPlayer";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <MediaPlayer meeting={meeting} />
